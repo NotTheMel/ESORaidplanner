b7dd1b77474a81302d7f477b8898746a4c0dfe64
Woeler <woeler@esoleaderboards.com>
2018-03-02 00:32:49 +0100

    Move timezone method to singleton class

--- app/Http/Controllers/Api/TimeZoneController.php
+++ app/Http/Controllers/Api/TimeZoneController.php
@@ -8,8 +8,7 @@
 
 namespace App\Http\Controllers\Api;
 
-use DateTime;
-use DateTimeZone;
+use App\Singleton\TimeZones;
 use Illuminate\Http\JsonResponse;
 
 class TimeZoneController extends ApiController
@@ -21,42 +20,6 @@ class TimeZoneController extends ApiController
      */
     public function getTimezones(): JsonResponse
     {
-        static $regions = [
-            DateTimeZone::AFRICA,
-            DateTimeZone::AMERICA,
-            DateTimeZone::ANTARCTICA,
-            DateTimeZone::ASIA,
-            DateTimeZone::ATLANTIC,
-            DateTimeZone::AUSTRALIA,
-            DateTimeZone::EUROPE,
-            DateTimeZone::INDIAN,
-            DateTimeZone::PACIFIC,
-        ];
-
-        $timezones = [];
-        foreach ($regions as $region) {
-            $timezones = array_merge($timezones, DateTimeZone::listIdentifiers($region));
-        }
-
-        $timezone_offsets = [];
-        foreach ($timezones as $timezone) {
-            $tz                          = new DateTimeZone($timezone);
-            $timezone_offsets[$timezone] = $tz->getOffset(new DateTime());
-        }
-
-        // sort timezone by offset
-        asort($timezone_offsets);
-
-        $timezone_list = [];
-        foreach ($timezone_offsets as $timezone => $offset) {
-            $offset_prefix    = $offset < 0 ? '-' : '+';
-            $offset_formatted = gmdate('H:i', abs($offset));
-
-            $pretty_offset = "UTC${offset_prefix}${offset_formatted}";
-
-            $timezone_list[$timezone] = "(${pretty_offset}) $timezone";
-        }
-
-        return response($timezone_list, 200);
+        return response(TimeZones::list(), 200);
     }
 }
