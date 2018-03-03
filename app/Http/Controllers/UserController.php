db203b1a4b817eadc26db31f427ef9d573907b73
Woeler <woeler@esoleaderboards.com>
2018-03-03 01:20:39 +0100

    Reuse timezone static

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -18,6 +18,7 @@ namespace App\Http\Controllers;
 use App\Avatar;
 use App\Character;
 use App\Set;
+use App\Singleton\TimeZones;
 use App\User;
 use DateTime;
 use DateTimeZone;
@@ -227,43 +228,7 @@ class UserController extends Controller
      */
     private function generate_timezone_list()
     {
-        static $regions = [
-            DateTimeZone::AFRICA,
-            DateTimeZone::AMERICA,
-            DateTimeZone::ANTARCTICA,
-            DateTimeZone::ASIA,
-            DateTimeZone::ATLANTIC,
-            DateTimeZone::AUSTRALIA,
-            DateTimeZone::EUROPE,
-            DateTimeZone::INDIAN,
-            DateTimeZone::PACIFIC,
-        ];
-
-        $timezones = [];
-        foreach ($regions as $region) {
-            $timezones = array_merge($timezones, DateTimeZone::listIdentifiers($region));
-        }
-
-        $timezone_offsets = [];
-        foreach ($timezones as $timezone) {
-            $tz                          = new DateTimeZone($timezone);
-            $timezone_offsets[$timezone] = $tz->getOffset(new DateTime());
-        }
-
-        // sort timezone by offset
-        asort($timezone_offsets);
-
-        $timezone_list = [];
-        foreach ($timezone_offsets as $timezone => $offset) {
-            $offset_prefix    = $offset < 0 ? '-' : '+';
-            $offset_formatted = gmdate('H:i', abs($offset));
-
-            $pretty_offset = "UTC${offset_prefix}${offset_formatted}";
-
-            $timezone_list[$timezone] = "(${pretty_offset}) $timezone";
-        }
-
-        return $timezone_list;
+        return TimeZones::list();
     }
 
     /**
