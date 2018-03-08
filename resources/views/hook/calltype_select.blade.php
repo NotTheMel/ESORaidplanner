d6a5db4594fd8ff79bb125040471821759d8ffea
Woeler <woeler@esoleaderboards.com>
2018-03-08 13:25:07 +0100

    Change header tags

--- resources/views/hook/calltype_select.blade.php
+++ resources/views/hook/calltype_select.blade.php
@@ -12,17 +12,17 @@
                     <div class="row">
                         <div class="col-md-4 text-center">
                             <a href="/hooks/typeselect/{{ \App\Singleton\HookTypes::ON_TIME }}">
-                                <h4>Create an event reminder notification</h4>
+                                <h5>Create an event reminder notification</h5>
                             </a>
                         </div>
                         <div class="col-md-4 text-center">
                             <a href="/hooks/typeselect/{{ \App\Singleton\HookTypes::ON_EVENT_CREATE }}">
-                                <h4>Create an event creation notification</h4>
+                                <h5>Create an event creation notification</h5>
                             </a>
                         </div>
                         <div class="col-md-4 text-center">
                             <a href="/hooks/typeselect/{{ \App\Singleton\HookTypes::ON_GUIDMEMBER_APPLICATION }}">
-                                <h4>Create a notification when someone applies to my guild</h4>
+                                <h5>Create a notification when someone applies to my guild</h5>
                             </a>
                         </div>
                     </div>
