e51f15c4ddbb528c6dde5008897bbfadb26df7c1
Woeler <woeler@esoleaderboards.com>
2018-07-18 22:44:21 +0200

    Add middleware for major refactoring

---
+++ app/Http/Middleware/GuildOwnerMiddleware.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Guild;
+use Auth;
+use Closure;
+
+class GuildOwnerMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure                 $next
+     *
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $slug = $request->route('slug');
+        /** @var Guild $guild */
+        $guild = Guild::query()->where('slug', '=', $slug)->first();
+
+        if (null === $guild || !$guild->isOwner(Auth::user())) {
+            return redirect('/');
+        }
+
+        return $next($request);
+    }
+}
